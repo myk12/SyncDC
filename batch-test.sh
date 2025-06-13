73ad38ba23317989060340d352cd6714cd48f34c
mayuke <mayuke803@gmail.com>
2025-06-13 16:20:03 +0000

    [test] batch test script

--- batch-test.sh
+++ batch-test.sh
@@ -10,27 +10,51 @@ if [ ! -f build/bin/sync-paxos ]; then
     exit 1
 fi
 
-# Change parameters and run the program
-
-# Sync Error Parameter Array
-sync_error_arr=(100000 10000 1000 100 10)
-# Message Delay Bound Array
-delay_bound_arr=(100000000 10000000 1000000 100000 10000)
-
 if [ -d result ]; then
     rm -rf result
     mkdir result
 fi
 
-for i in "${sync_error_arr[@]}"
-do
-    for j in "${delay_bound_arr[@]}"
-    do
-        mkdir -p result/SyncErr_$i/MsgDelay_$j
+################################################
+#        Synchronous Paxos
+################################################    
+# Change parameters and run the program
+# Message Delay Bound Array
+delay_bound_arr=(100000 10000 1000 100 10)
+# Sync Error Parameter Array
+sync_error_arr=(100000 10000 1000 100 10)
 
-        ./build/bin/sync-paxos --clockSyncError=${i}ns --boundedMessageDelay=${j}ns
+mkdir -p result/Sync
+
+for i in "${delay_bound_arr[@]}"
+do
+    for j in "${sync_error_arr[@]}"
+    do
+        mkdir -p result/Sync/Delay_${i}us/Sync_${j}ns
+        
+        ./build/bin/sync-paxos --sync=1 --clockSyncError=${j}ns --boundedMessageDelay=${i}us
 
         # move the result to the result folder
-        mv *.dat result/SyncErr_$i/MsgDelay_$j/
+        mv *.dat result/Sync/Delay_${i}us/Sync_${j}ns/
     done
+done
+
+################################################
+#        Asynchronous Paxos
+################################################
+
+# Change parameters and run the program
+# Message Delay
+delay_arr=(100000 10000 1000 100 10)
+
+mkdir -p result/Async
+
+for i in "${delay_arr[@]}"
+do
+    mkdir -p result/Async/Delay_${i}us
+    
+    ./build/bin/sync-paxos --sync=0 --linkDelay=${i}us 
+
+    # move the result to the result folder
+    mv *.dat result/Async/Delay_${i}us/
 done
